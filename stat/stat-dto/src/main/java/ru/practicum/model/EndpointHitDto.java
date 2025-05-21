054f70fc98b9f419ac77520556b7740e0bd68f9e
Andrey Pesterev <andrey1455@gmail.com>
2025-05-21 18:54:39 +0300

    Исправлены замечания к BaseClient, StatsClient, EndpointHitDto

--- stat/stat-dto/src/main/java/ru/practicum/model/EndpointHitDto.java
+++ stat/stat-dto/src/main/java/ru/practicum/model/EndpointHitDto.java
@@ -1,5 +1,6 @@
-package ru.practicum.model;
+package ru.practicum;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,5 +14,7 @@ public class EndpointHitDto {
 	private String app;
 	private String uri;
 	private String ip;
+
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
 	private LocalDateTime timestamp;
 }
